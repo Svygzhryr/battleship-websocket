f8f8578564cb8fa32258c9b1c27c5c556fa37fcc
Svygzhryr <pamixy@gmail.com>
2024-02-18 15:10:06 +0300

    feat: finish correct room creating

--- src-ts/commands/index.ts
+++ src-ts/commands/index.ts
@@ -1,94 +1,110 @@
-import { WebSocket, WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from "ws";
 
-import { ICommand, IPlayer, IRoom, ISocketData } from '../types';
-import { players, winners, rooms } from '../storage';
-import { generatePlayerId, generateRoomId } from '../utils';
-import { updateRooms, updateWinners } from './utility';
+import { ICommand, IPlayer, IRoom, ISocketData } from "../types";
+import { players, winners, rooms } from "../storage";
+import { generatePlayerId, generateRoomId } from "../utils";
+import { updateRooms, updateWinners } from "./utility";
 
 const REG = (socketData: ISocketData) => {
   const { data, ws, wss, id } = socketData;
-  console.log('>>reg', data);
+  console.log(">>reg", data);
   const user: IPlayer = JSON.parse(data.data);
   user.id = id;
   players.push(user);
 
   const responseData = JSON.stringify({
-    type: 'reg',
+    type: "reg",
     data: JSON.stringify({
       name: user.name,
       index: id,
       error: false,
-      errorText: ''
+      errorText: "",
     }),
-    id: 0
+    id: 0,
   });
 
   ws.send(responseData);
-  console.log(players);
-
   updateRooms(wss);
   updateWinners(wss);
 };
 
+const ADD_USER_TO_ROOM = (socketData: ISocketData, roomId: string) => {
+  const { data, ws, wss, id } = socketData;
+
+  if (!roomId) {
+    roomId = JSON.parse(data.data).indexRoom;
+    console.log(">>addUserToRoom", data);
+  }
+
+  rooms.find((room) => {
+    room.roomId === roomId;
+    room.roomUsers.push();
+  });
+
+  const responseData = JSON.stringify({
+    type: "add_user_to_room",
+    data: JSON.stringify({
+      indexRoom: roomId,
+    }),
+
+    id: 0,
+  });
+
+  ws.send(responseData);
+  updateRooms(wss);
+};
+
 const CREATE_ROOM = (socketData: ISocketData) => {
   const { data, ws, wss, id } = socketData;
-  console.log('>>createRoom', data);
+  console.log(id);
+  console.log(">>createRoom", data);
 
+  const foundPlayer = players.find((player) => player.id === id);
+  console.log(">> found player", foundPlayer);
   const roomId = generateRoomId();
   const newRoom: IRoom = {
     roomId,
     roomUsers: [
       // get socket id and use here
       {
-        name: lastPlayer.name,
-        index: lastPlayer.id
-      }
-    ]
+        name: foundPlayer.name,
+        index: foundPlayer.id,
+      },
+    ],
   };
   rooms.push(newRoom);
-  updateRooms(wss);
-};
-
-const ADD_USER_TO_ROOM = (socketData: ISocketData) => {
-  const { data, ws, wss, id } = socketData;
-
-  const roomId = JSON.parse(data.data).indexRoom;
-  console.log('>>addUserToRoom', data);
-
-  // rooms.find((room) => {
-  //   room.roomId === roomId;
-  //   room.roomUsers.push();
-  // });
-
-  // ws.send(responseData);
+  ADD_USER_TO_ROOM(socketData, roomId);
   updateRooms(wss);
 };
 
 const ADD_SHIPS = (socketData: ISocketData) => {
-  console.log('add ships');
+  console.log("add ships");
 };
 
 const ATTACK = (socketData: ISocketData) => {
-  console.log('attack');
+  console.log("attack");
 };
 
 const RANDOM_ATTACK = (socketData: ISocketData) => {
-  console.log('randomAttack');
+  console.log("randomAttack");
 };
 
 const TURN = (socketData: ISocketData) => {
-  console.log('turn');
+  console.log("turn");
 };
 
 const FINISH = (socketData: ISocketData) => {
-  console.log('finish');
+  console.log("finish");
 };
 
-export const commands: Record<string, (socketData: ISocketData) => void> = {
+export const commands: Record<
+  string,
+  (socketData: ISocketData, roomId?: string) => void
+> = {
   REG,
   ADD_USER_TO_ROOM,
   CREATE_ROOM,
   TURN,
   ATTACK,
-  FINISH
+  FINISH,
 };
--- src-ts/commands/utility.ts
+++ src-ts/commands/utility.ts
@@ -1,15 +1,15 @@
 // for the convenience i moved here functions
 // that are only invoked on a server side
-import ws from 'ws';
+import ws from "ws";
 
-import { WebSocket, WebSocketServer } from 'ws';
-import { rooms, winners } from '../storage';
+import { WebSocket, WebSocketServer } from "ws";
+import { rooms, winners } from "../storage";
 
 export const updateWinners = (wss: WebSocketServer) => {
   const responseData = JSON.stringify({
-    type: 'update_winners',
+    type: "update_winners",
     data: JSON.stringify(winners),
-    id: 0
+    id: 0,
   });
 
   wss.clients.forEach((client) => {
@@ -20,12 +20,13 @@ export const updateWinners = (wss: WebSocketServer) => {
 export const updateRooms = (wss: WebSocketServer) => {
   wss.clients.forEach((client) => {
     const responseData = {
-      type: 'update_room',
+      type: "update_room",
       data: JSON.stringify(rooms),
-      id: 0
+      id: 0,
     };
     client.send(JSON.stringify(responseData));
   });
+  console.log(">> all rooms", rooms, JSON.parse(rooms.data));
 };
 
 export const createGame = () => {};
